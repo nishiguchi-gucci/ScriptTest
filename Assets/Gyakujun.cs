5c2ca328b0a0e59e8b77990d15ae3a6e26fed92c
nishiguchi-gucci <ah3yr6gucci@gmail.com>
2020-06-29 22:55:05 +0900

    Lesson4のScriptTestの課題提出です。

---
+++ Assets/Gyakujun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyakujun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = new int[5];
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Last.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+        Debug.Log("残りHPは" + this.hp);
+    }
+
+    public void Magic(int magic)
+    {
+        //単独処理
+        //if (this.mp > magic)
+        //{
+            //this.mp -= magic;
+            //Debug.Log("魔法攻撃した。残りMPは" + mp);
+        //}
+        //else
+        //{
+            //Debug.Log("MPが足りないため魔法が使えない");
+        //}
+
+        //連続処理
+        for (int i = 0; i < 11; i++)
+        {
+            if (this.mp > magic)
+            {
+                this.mp -= magic;
+                Debug.Log("魔法攻撃した。残りMPは" + this.mp);
+            }
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない");
+            } 
+        }
+    }
+}
+public class Last : MonoBehaviour
+{
+
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+        lastboss.Defence(3);
+        lastboss.Magic(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
